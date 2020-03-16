0e7454c3a19df0a681810abe3c0570b42b5044f3
Flynn Kearney <40428486@live.napier.ac.uk>
2020-03-16 11:48:41 +0000

    Started CapitalCityReportTest.

---
+++ src/test/java/com/napier/reports/CapitalCityReportTest.java
@@ -0,0 +1,12 @@
+package com.napier.reports;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CapitalCityReportTest {
+
+    @Test
+    public void CapitalCityReportTest() {
+
+    }
+}
