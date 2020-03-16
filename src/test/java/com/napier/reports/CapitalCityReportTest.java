feb8fe916518b001b4a3155eac235b2e0d54a1c3
Flynn Kearney <40428486@live.napier.ac.uk>
2020-03-16 12:02:17 +0000

    Completed CapitalCityReportTest.

--- src/test/java/com/napier/reports/CapitalCityReportTest.java
+++ src/test/java/com/napier/reports/CapitalCityReportTest.java
@@ -7,6 +7,14 @@ public class CapitalCityReportTest {
 
     @Test
     public void CapitalCityReportTest() {
+        CapitalCityReport testee = new CapitalCityReport("Canberra", "Australia", 322723);
+        assertEquals("Canberra", testee.getName());
+        assertEquals("Australia", testee.getCountry());
+        assertEquals(322723, (int)testee.getPopulation());
+    }
 
+    @Test
+    public void illegalCapitalCityReportTest() {
+        assertThrows(IllegalArgumentException.class,() -> new CapitalCityReport("Canberra", "Australia", 322723));
     }
 }
